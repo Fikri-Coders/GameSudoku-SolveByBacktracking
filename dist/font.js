37fa5c252a5528d12f7563a1da0d571ae47ff417
Fikri-Coders <fikrind421@gmail.com>
2025-05-18 19:27:41 +0700

    Deploy application to Netlify

---
+++ dist/font.js
@@ -0,0 +1,13 @@
+// Load Google Font
+(function() {
+  const link = document.createElement('link');
+  link.href = "https://fonts.googleapis.com/css2?family=Pacifico&family=Grandstander:wght@400;600&display=swap";
+  link.rel = "stylesheet";
+  document.head.appendChild(link);
+
+  // Load Font Awesome
+  const fontAwesome = document.createElement('link');
+  fontAwesome.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css";
+  fontAwesome.rel = "stylesheet";
+  document.head.appendChild(fontAwesome);
+})();
