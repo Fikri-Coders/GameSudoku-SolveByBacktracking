ee2bb6029963219950690bc9b275f92070126db9
Fikri-Coders <fikrind421@gmail.com>
2025-05-18 19:35:23 +0700

    Mengimplementasikan Tema FinTrack

--- dist/font.js
+++ dist/font.js
@@ -1,7 +1,7 @@
 // Memuat Font Google
 (function() {
   const link = document.createElement('link');
-  link.href = "https://fonts.googleapis.com/css2?family=Pacifico&family=Grandstander:wght@400;600&display=swap";
+  link.href = "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap";
   link.rel = "stylesheet";
   document.head.appendChild(link);
 
